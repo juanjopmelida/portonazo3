bf70e939ea18d33e6e46baa47904b14ae8819b43
Juanjo <jperez@viasatelematics.com>
2021-06-02 17:38:55 +0200

    App name and splashscreen

--- views/SplashScreen.js
+++ views/SplashScreen.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, Image, StyleSheet} from 'react-native';
 import {useTheme} from '@react-navigation/native';
 
 export function SplashScreen() {
   const {colors} = useTheme();
-  return <View style={[styles.container, {backgroundColor: colors.primary}]} />;
+  return (
+    <View style={[styles.container, {backgroundColor: colors.primary}]}>
+      <Image source={require('../assets/logoViasatTelematicsW250.png')} />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
