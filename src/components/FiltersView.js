8d49bca724e1a528b3aeebfa2335ef7e6373581c
Juanjo <jperez@viasatelematics.com>
2021-09-09 11:57:50 +0200

    Styling FiltersView

--- src/components/FiltersView.js
+++ src/components/FiltersView.js
@@ -1,29 +1,38 @@
 import moment from 'moment';
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {useTheme} from '@react-navigation/native';
 
 export default function FiltersView(props) {
   const {filters} = props;
+  const {colors} = useTheme();
+
   return (
-    <View style={styles.filtersViewContainer}>
-      <View>
-        <Text>{filters.vehiclePlate}</Text>
-      </View>
-      <View>
-        <Text>{moment(filters.startDate).format('DD/MM/YY HH:mm')}</Text>
-      </View>
-      <View>
-        <Text>{moment(filters.endDate).format('DD/MM/YY HH:mm')}</Text>
-      </View>
+    <View
+      style={[
+        styles.filtersViewContainer,
+        {backgroundColor: colors.background, borderColor: colors.border},
+      ]}>
+      <Text style={[styles.filtersViewText, {color: colors.primary}]}>
+        {`${filters.vehiclePlate} | ${moment(filters.startDate).format(
+          'DD/MM/YYYY',
+        )} - ${moment(filters.endDate).format('DD/MM/YYYY')}`}
+      </Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   filtersViewContainer: {
-    height: 30,
+    height: 20,
     flexDirection: 'row',
     justifyContent: 'space-evenly',
     alignItems: 'center',
+    backgroundColor: '#f2f2f2',
+    borderBottomWidth: 0.5,
+    borderBottomColor: '#cacaca',
+  },
+  filtersViewText: {
+    fontSize: 11,
   },
 });
